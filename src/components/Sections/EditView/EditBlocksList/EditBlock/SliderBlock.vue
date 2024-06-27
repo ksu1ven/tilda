dc3224f0ed1d44ba09788e30a59638ea48b7d707
Oksana <ksu1ven@mail.ru>
2024-06-28 01:47:12 +0300

    feat: finish swiper editable and add drag and drop

--- src/components/Sections/EditView/EditBlocksList/EditBlock/SliderBlock.vue
+++ src/components/Sections/EditView/EditBlocksList/EditBlock/SliderBlock.vue
@@ -1,29 +1,39 @@
 <template>
     <div class="edit-block-slider">
-        <BlockWrapper v-if="contenteditable" :index />
+        <BlockWrapper v-if="contenteditable" :index="index" />
         <div class="edit-block-slider__content">
-            <div
-                v-for="(image, index) in imageArr"
-                :key="index"
-                class="edit-block-slider__img-container"
+            <draggable
+                v-model="currentImageArr"
+                itemKey="id"
+                class="edit-block-slider__content__list"
+                @end="onDragEnd"
             >
-                <img :src="image" alt="Slide" class="background-img" />
-            </div>
+                <template #item="{ element }">
+                    <div :key="element.id" class="edit-block-slider__img-container">
+                        <img :src="element.src" alt="Slide" class="background-img" />
+                    </div>
+                </template>
+            </draggable>
         </div>
     </div>
 </template>
 
 <script>
+import { useProjectsStore } from '../../../../../stores'
+import { mapActions } from 'pinia'
 import BlockWrapper from './BlockWrapper.vue'
+import draggable from 'vuedraggable'
 
 export default {
-    components: { BlockWrapper },
+    components: {
+        BlockWrapper,
+        draggable
+    },
     props: {
         index: {
             type: Number,
             required: true
         },
-
         contenteditable: {
             type: Boolean,
             required: true
@@ -37,7 +47,25 @@ export default {
         return {
             projectId: +this.$route.params.projectId,
             pageId: +this.$route.params.pageId,
-            currentImageArr: []
+            currentImageArr: this.imageArr.map((src, id) => ({ id, src }))
+        }
+    },
+    methods: {
+        ...mapActions(useProjectsStore, ['editPageContent']),
+        onDragEnd(event) {
+            let imageArrCopy = this.imageArr.slice(0)
+            let spliced = imageArrCopy.splice(event.oldIndex, 1)
+            imageArrCopy.splice(event.newIndex, 0, spliced[0])
+            this.editPageContent(this.projectId, this.pageId, this.index, {
+                imageArr: imageArrCopy
+            })
+        }
+    },
+    watch: {
+        imageArr: {
+            handler() {
+                this.currentImageArr = this.imageArr.map((src, id) => ({ id, src }))
+            }
         }
     }
 }
